83010067fb8ffb86412a9c675570722059fd38a7
Zydj <charles.desjardins96@gmail.com>
2021-03-12 12:09:11 -0500

    Added the patch files to handle task changes
    Isolated where the task bar changes locally and for all players. Also found where the number of task is set.

---
+++ Jester/Jester/GameOptionsDataPatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+
+namespace Jester
+{
+    [HarmonyPatch(typeof(GameOptionsData))]
+    public static class GameOptionsDataPatch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch("NHJLMAAHKJF")]
+        public static void Postfix(GameOptionsData __instance, ref string __result, int MKGPLPMAKLO)
+        {
+            /*
+            Jester.log.LogMessage("ShortTasks: " + __instance.NumShortTasks);
+            Jester.log.LogMessage("LongTasks: " + __instance.NumLongTasks);
+            Jester.log.LogMessage("CommonTasks: " + __instance.NumCommonTasks);     
+            */      
+            
+        }
+    }
+}
---
+++ Jester/Jester/NormalPlayerTaskPatch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+
+namespace Jester
+{
+    [HarmonyPatch(typeof(NormalPlayerTask), nameof(NormalPlayerTask.Complete))]
+    public static class NormalPlayerTaskPatch
+    {
+        public static void Prefix()
+        {
+            if (!PlayerController.LocalPlayer.hasComponent("Jester"))
+            {
+                return;
+            }
+            
+            Jester.log.LogMessage("Do not update local task bar");
+        }
+    }
+}
