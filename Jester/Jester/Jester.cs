b2a45022e5ebc05ddd6190585c851349afcc6327
Zydj <charles.desjardins96@gmail.com>
2021-03-17 02:25:50 -0400

    Added a debugging flag and removed unused functions

--- Jester/Jester/Jester.cs
+++ Jester/Jester/Jester.cs
@@ -18,21 +18,23 @@ namespace Jester
     [BepInDependency(ReactorPlugin.Id)]
     public class Jester : BasePlugin
     {
+        public static bool debug = false;
+
         public static bool jesterWon = false;
         public const string Id = "org.bepinex.plugins.Jester";
         public static BepInEx.Logging.ManualLogSource log;
 
         public static PlayerControl localPlayer = null;
         public static List<PlayerControl> localPlayers = new List<PlayerControl>();
+
         public static bool introDone = false;
         public static bool jesterEnabled = true;
+
         public static Player lastPlayerTask = null;
 
         public static Color jesterColor = new Color(1, (float)(63.0 / 100), (float)(72.0 / 100));
 
         public Harmony Harmony { get; } = new Harmony(Id);
-
-        public ConfigEntry<string> Name { get; private set; }
 
         public override void Load()
         {
