9cbd8bc83efa8f6c7842ac8458df36ba959b9b87
Zydj <charles.desjardins96@gmail.com>
2021-03-17 21:39:03 -0400

    Changed the code for compatibility
    Changed where we got the value of the last component is the menu option list. We now get it in the Start method instead of the update

--- Jester/Jester/GameOptionsMenuPatch.cs
+++ Jester/Jester/GameOptionsMenuPatch.cs
@@ -8,23 +8,32 @@ namespace Jester
     {
         public static ToggleOption showJesterOption;
         public static GameOptionsMenu instance;
+        
+        public static OptionBehaviour option;
+
+        public static float defaultBounds;
 
         [HarmonyPatch(typeof(GameOptionsMenu), nameof(GameOptionsMenu.Start))]
         public static void Postfix(GameOptionsMenu __instance)
         {
             instance = __instance;
-            CustomPlayerMenuPatch.AddOptions();
+
+            defaultBounds = __instance.GetComponentInParent<Scroller>().YBounds.max;
+
+            option = __instance.Children[__instance.Children.Count - 1];
+
+            CustomPlayerMenuPatch.AddOptions(); 
         }
 
         [HarmonyPostfix]
         [HarmonyPatch(typeof(GameOptionsMenu), nameof(GameOptionsMenu.Update))]
         public static void Postfix1(GameOptionsMenu __instance)
         {
-            OptionBehaviour option = __instance.Children[__instance.Children.Count - 2];
-
             if (showJesterOption != null)
             {
                 showJesterOption.transform.position = option.transform.position - new Vector3(0, 0.5f, 0);
+
+                __instance.GetComponentInParent<Scroller>().YBounds.max = defaultBounds + 0.5f;
             }
         }
     }
